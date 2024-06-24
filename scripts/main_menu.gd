ef3b0671f33e2cba57715bba8ff2bd0f4b3c0c37
zlac212 <zlac212@live.rhul.ac.uk>
2024-06-24 15:32:15 +0100

    added main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
-
+var scene = preload("res://scenes/main_game.tscn").instantiate()
 
 
 func _on_play_game_pressed():
-	get_tree().change_scene("res://scenes/main_game.tscn")
+	get_tree().root.add_child(scene)
+	get_tree().root.remove_child(self)
+
+
+func _on_exit_pressed():
+	get_tree().quit()
